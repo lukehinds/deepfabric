4e2c8a6495bdf68b6ee6cc98830f5ed4473cf085
Luke Hinds <lukehinds@gmail.com>
2025-12-08 08:10:23 +0000

    Fix missing API Key checks (#465)
    * Fix missing API Key checks
    
    Signed-off-by: Luke Hinds <lukehinds@gmail.com>
    
    * Code optimization for key api checks
    
    ---------
    
    Signed-off-by: Luke Hinds <lukehinds@gmail.com>

--- deepfabric/llm/__init__.py
+++ deepfabric/llm/__init__.py
@@ -1,5 +1,18 @@
 """LLM abstraction layer for DeepFabric."""
 
+from .api_key_verifier import (
+    VerificationResult,
+    VerificationStatus,
+    verify_all_api_keys,
+    verify_all_api_keys_async,
+    verify_anthropic_api_key,
+    verify_gemini_api_key,
+    verify_ollama_connection,
+    verify_openai_api_key,
+    verify_openrouter_api_key,
+    verify_provider_api_key,
+    verify_provider_api_key_async,
+)
 from .client import (
     PROVIDER_API_KEY_MAP,
     LLMClient,
@@ -11,7 +24,18 @@ from .client import (
 __all__ = [
     "LLMClient",
     "PROVIDER_API_KEY_MAP",
+    "VerificationResult",
+    "VerificationStatus",
     "get_required_api_key_env_var",
     "make_outlines_model",
     "validate_provider_api_key",
+    "verify_all_api_keys",
+    "verify_all_api_keys_async",
+    "verify_anthropic_api_key",
+    "verify_gemini_api_key",
+    "verify_ollama_connection",
+    "verify_openai_api_key",
+    "verify_openrouter_api_key",
+    "verify_provider_api_key",
+    "verify_provider_api_key_async",
 ]
